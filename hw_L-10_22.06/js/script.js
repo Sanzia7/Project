8a07b124474bf2824070a1e3d413f317ab3ed28b
Sanzia7 <meritee@gmx.de>
2022-06-26 16:14:51 +0200

    correction of js code and styles

--- hw_L-10_22.06/js/script.js
+++ hw_L-10_22.06/js/script.js
@@ -1,17 +1,18 @@
-const product_add_form_elem = document.querySelector(".product_form");
-let products = [];
+const product_add_form_elem = document.querySelector(".products_form");
+let productsArr = [];
 
 function render() {
-   const container = document.querySelector(".product-container");
-   container.innerText = "";
-   products.forEach(({ title, price }) => {
+   const productsContainer = document.querySelector(".products_container");
+   // productsContainer.innerText = "";
+   productsArr.forEach(({ title, price }) => {
       const product = document.createElement("div");
       const titleElem = document.createElement("p");
       const priceElem = document.createElement("p");
       titleElem.innerText = title;
       priceElem.innerText = price;
+      product.classList.add('product');
       product.append(titleElem, priceElem);
-      container.append(product);
+      productsContainer.append(product);
    });
 }
 
@@ -19,7 +20,7 @@ product_add_form_elem.addEventListener("submit", (event) => {
    event.preventDefault();
    const { title, price } = event.target;
 
-   products.push({
+   productsArr.push({
       title: title.value,
       price: price.value,
    });
